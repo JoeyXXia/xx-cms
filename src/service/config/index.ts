363ec3aef0064f61e6c4cb0bc9e2534114e042bc
Xu <whitenight.xiaxu@gmail.com>
2023-07-18 16:46:08 +0800

    feat: prod dev config

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -1,2 +1,10 @@
-export const BASE_URL = 'http://152/136.185.210:4000'
+let BASE_URL = ''
+
+if (import.meta.env.PROD) {
+  BASE_URL = 'http://152/136.185.210:4000'
+} else {
+  BASE_URL = 'http://152/136.185.210:5000'
+}
 export const TIME_OUT = 10000
+
+export { BASE_URL }

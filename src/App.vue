1ebcaf50d5d8ed154f24b1c9e2c9ea58d0c469d3
Xu <whitenight.xiaxu@gmail.com>
2023-07-20 16:20:50 +0800

    feat: login background

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h2>hello</h2>
-  <router-view></router-view>
-  <el-row class="mb-4">
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-  </el-row>
+  <div class="app">
+    <router-view></router-view>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>login</div>
+  <div class="login"><login-panel /></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LoginPanel from './cpns/login-panel.vue'
+</script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.login {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100%;
+
+  background: url('../../assets/img/login-bg.svg');
+}
+</style>

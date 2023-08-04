f2bfe487ae17b8713919302b738568c8e07b5ae6
Xu <whitenight.xiaxu@gmail.com>
2023-08-04 17:25:04 +0800

    feat: cache token

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -1,11 +1,14 @@
 import { defineStore } from 'pinia'
 import { accountLoginRequest } from '@/service/login/login'
 import type { IAccount } from '@/types'
+import { localCache, sessionCache } from '@/utils/cache'
+
+const LOGIN_TOKEN = 'login-token'
 
 const userLoginStore = defineStore('login', {
   state: () => ({
     id: '',
-    token: '',
+    token: localCache.getCache(LOGIN_TOKEN) ?? '',
     name: ''
   }),
   actions: {
@@ -14,6 +17,8 @@ const userLoginStore = defineStore('login', {
       this.id = longinResult.data.id
       this.name = longinResult.data.name
       this.token = longinResult.data.token
+
+      localCache.setCache(LOGIN_TOKEN, this.token)
     }
   }
 })

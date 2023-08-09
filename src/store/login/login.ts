995851bbfaddd8d94f5509ee69d9d0ea40c9ff36
Xu <whitenight.xiaxu@gmail.com>
2023-08-09 16:41:47 +0800

    feat: add request token

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { accountLoginRequest } from '@/service/login/login'
+import { accountLoginRequest, getUserInfoById } from '@/service/login/login'
 import type { IAccount } from '@/types'
 import { localCache, sessionCache } from '@/utils/cache'
 import router from '@/router'
@@ -7,18 +7,19 @@ import { LOGIN_TOKEN } from '@/global/constants'
 
 const userLoginStore = defineStore('login', {
   state: () => ({
-    id: '',
     token: localCache.getCache(LOGIN_TOKEN) ?? '',
-    name: ''
+    userInfo: localCache.getCache('userInfo') ?? {}
   }),
   actions: {
     async loginAccountAction(account: IAccount) {
       const longinResult = await accountLoginRequest(account)
-      this.id = longinResult.data.id
-      this.name = longinResult.data.name
+      const id = longinResult.data.id
       this.token = longinResult.data.token
-
       localCache.setCache(LOGIN_TOKEN, this.token)
+
+      const userInfoResult = await getUserInfoById(id)
+      const userInfo = userInfoResult.data
+      this.userInfo = userInfo
 
       router.push('/main')
     }

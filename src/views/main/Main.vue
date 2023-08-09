0c706d92d2a35ac5398ea9c44400000097a29e2f
Xu <whitenight.xiaxu@gmail.com>
2023-08-09 15:41:50 +0800

    feat: logout logic

--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -1,7 +1,18 @@
 <template>
   <div>main</div>
+  <button @click="logoutBtn">退出登录</button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { LOGIN_TOKEN } from '@/global/constants'
+import { localCache } from '@/utils/cache'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+function logoutBtn() {
+  localCache.removeCache(LOGIN_TOKEN)
+  router.push('/login')
+}
+</script>
 
 <style lang="less" scoped></style>

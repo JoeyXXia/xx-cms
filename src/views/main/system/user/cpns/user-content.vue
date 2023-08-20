10fa19a048cc5f6de27de8ff45580d7de214026b
Xu <whitenight.xiaxu@gmail.com>
2023-08-20 17:50:04 +0800

    feat: basic user content

---
+++ src/views/main/system/user/cpns/user-content.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="content">
+    <div class="header">
+      <h3 class="title">用户列表</h3>
+      <el-button type="primary">新建用户</el-button>
+    </div>
+    <div class="table">
+      <el-table style="width: 100%" border>
+        <el-table-column align="center" type="selection" width="60px" />
+        <el-table-column align="center" type="index" label="序号" width="60px" />
+        <el-table-column align="center" prop="name" label="用户名" width="150px" />
+        <el-table-column align="center" prop="realname" label="真实姓名" width="150px" />
+        <el-table-column align="center" prop="cellphone" label="手机号码" width="150px" />
+        <el-table-column align="center" prop="enable" label="状态" width="100px"> </el-table-column>
+      </el-table>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped></style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="user">
     <user-search />
+    <user-content />
   </div>
 </template>
 
 <script setup lang="ts" name="user">
 import UserSearch from './cpns/user-search.vue'
+import UserContent from './cpns/user-content.vue'
 </script>
 
 <style scoped>

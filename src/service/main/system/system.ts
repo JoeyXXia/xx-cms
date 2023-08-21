f38096b0efe25bfb12834850162d8af99af2195c
Xu <whitenight.xiaxu@gmail.com>
2023-08-21 10:08:00 +0800

    feat: system user page request&store

---
+++ src/service/main/system/system.ts
@@ -0,0 +1,58 @@
+import xxRequest from '@/service'
+
+// user nework request
+export function postUsersListData(queryInfo: any) {
+  return xxRequest.post({
+    url: '/users/list',
+    data: queryInfo
+  })
+}
+
+export function deleteUserById(id: number) {
+  return xxRequest.post({
+    url: `/users/${id}`
+  })
+}
+
+export function newUsersData(userInfo: any) {
+  return xxRequest.post({
+    url: '/users',
+    data: userInfo
+  })
+}
+
+export function editUserData(id: number, userInfo: any) {
+  return xxRequest.post({
+    url: `/users/${id}`,
+    data: userInfo
+  })
+}
+
+//page request
+
+export function postPageListData(pageName: any, queryInfo: any) {
+  return xxRequest.post({
+    url: `/${pageName}/list`,
+    data: queryInfo
+  })
+}
+
+export function deletePageById(pageName: string, id: number) {
+  return xxRequest.post({
+    url: `/${pageName}/${id}`
+  })
+}
+
+export function newPageData(pageName: string, pageInfo: any) {
+  return xxRequest.post({
+    url: `${pageName}`,
+    data: pageInfo
+  })
+}
+
+export function editPageData(pageName: string, id: number, pageInfo: any) {
+  return xxRequest.post({
+    url: `/${pageName}/${id}`,
+    data: pageInfo
+  })
+}
---
+++ src/store/main/sysytem/system.ts
@@ -0,0 +1,71 @@
+import {
+  deletePageById,
+  deleteUserById,
+  editPageData,
+  editUserData,
+  newPageData,
+  newUsersData,
+  postPageListData,
+  postUsersListData
+} from '@/service/main/system/system'
+import { defineStore } from 'pinia'
+import type { ISystemState } from './type'
+import { deletePageById } from '../../../service/main/system/system'
+
+const userSystemStore = defineStore('system', {
+  state: (): ISystemState => ({
+    usersList: [],
+    usersTotalCount: 0,
+
+    pageList: [],
+    pageTotalCount: 0
+  }),
+  actions: {
+    // user data
+    async postUsersListAction(queryInfo: any) {
+      const usersListResult = await postUsersListData(queryInfo)
+      const { totalCount, list } = usersListResult.data
+      this.usersTotalCount = totalCount
+      this.usersList = list
+    },
+    async deleteUserByIdAction(id: number) {
+      const delteResult = await deleteUserById(id)
+      console.log(delteResult)
+      this.postUsersListAction({ offset: 0, size: 10 })
+    },
+    async newUserDataAction(userInfo: any) {
+      const newResult = await newUsersData(userInfo)
+      console.log(newResult)
+      this.postUsersListAction({ offset: 0, size: 10 })
+    },
+    async editUserDataAction(id: number, userInfo: any) {
+      const editResult = await editUserData(id, userInfo)
+      console.log(editResult)
+      this.postUsersListAction({ offset: 0, size: 10 })
+    },
+    // page data
+    async postPageListDataAction(pageName: string, queryInfo: any) {
+      const pageListResult = await postPageListData(pageName, queryInfo)
+      const { totalCount, list } = pageListResult.data
+      this.pageList = list
+      this.pageTotalCount = totalCount
+    },
+    async deletePageByIdAction(pageName: string, id: number) {
+      const deleteResult = await deletePageById(pageName, id)
+      console.log(deleteResult)
+      this.postPageListDataAction(pageName, { offset: 0, size: 10 })
+    },
+    async newPageDataAction(pageName: string, pageInfo: any) {
+      const newResult = await newPageData(pageName, pageInfo)
+      console.log(newResult)
+      this.postPageListDataAction(pageName, { offset: 0, size: 10 })
+    },
+    async editPageDataAction(pageName: string, id: number, pageInfo: any) {
+      const editResult = await editPageData(pageName, id, pageInfo)
+      console.log(editResult)
+      this.postPageListDataAction(pageName, { offset: 0, size: 10 })
+    }
+  }
+})
+
+export default userSystemStore

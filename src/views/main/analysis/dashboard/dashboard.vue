86eee6edda7cd09afb83824289745afdf66bbbc1
Xu <whitenight.xiaxu@gmail.com>
2024-07-29 15:17:34 +0800

    feat: dashborad store data

--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="dashboard">
+    <statistical-panel />
     <el-row :gutter="10">
-      <template>
-        <el-col :md="12" :lg="6" :xl="6"> </el-col>
+      <template v-for="item in topPanelData" :key="item.title">
+        <el-col :md="12" :lg="6" :xl="6">
+          <statistical-panel />
+        </el-col>
       </template>
     </el-row>
   </div>
@@ -10,11 +13,42 @@
 
 <script setup lang="ts" name="dashboard">
 import useAnalysisStore from '@/store/main/analysis/analysis'
+import { computed } from 'vue'
+import statisticalPanel from '@/components/statistical-panel/statistical-panel.vue'
 
 const analysisStore = useAnalysisStore()
+
+// top PanelData
+const topPanelData = computed(() => analysisStore.topPanelDatas)
+const categoryGoodsCount = computed(() => {
+  return analysisStore.categoryGoodsCount.map((item) => {
+    return { value: item.goodsCount, name: item.name }
+  })
+})
+const categoryGoodSale = computed(() => {
+  const goodsSale = analysisStore.categoryGoodsSale
+  const labels: string[] = []
+  const values: any[] = []
+
+  for (const item of goodsSale) {
+    labels.push(item.name)
+    values.push(item.goodsCount)
+  }
+  return { labels, values }
+})
+const addressGoodSale = computed(() => {
+  return analysisStore.goodsAddressSale.map((item) => {
+    return { name: item.address, value: item.count }
+  })
+})
 </script>
 
 <style scoped>
 .dashboard {
+  background-color: #f5f5f5;
+
+  .row {
+    margin-top: 20px;
+  }
 }
 </style>

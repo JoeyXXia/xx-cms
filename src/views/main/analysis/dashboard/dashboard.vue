a5f0e1b8be516f8ed32295e1cdf9f57982b512b3
Xu <whitenight.xiaxu@gmail.com>
2024-08-06 15:47:12 +0800

    feat: dashboard statiscal panel

--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="dashboard">
     <el-row :gutter="10">
-      <template v-for="item in topPanelData" :key="item.title">
+      <template v-for="item in topPanelDatas" :key="item.title">
         <el-col :md="12" :lg="6" :xl="6">
-          <statistical-panel :panel-data="item" />
+          <statistical-panel v-bind="item" />
         </el-col>
       </template>
     </el-row>
@@ -14,32 +14,26 @@
 import useAnalysisStore from '@/store/main/analysis/analysis'
 import { computed } from 'vue'
 import statisticalPanel from '@/components/statistical-panel/statistical-panel.vue'
+import { storeToRefs } from 'pinia'
 // store dispatch network
 const analysisStore = useAnalysisStore()
 analysisStore.getAnalysisDataAction()
 
 // top PanelData
-const topPanelData = computed(() => analysisStore.topPanelDatas)
-const categoryGoodsCount = computed(() => {
-  return analysisStore.categoryGoodsCount.map((item) => {
-    return { value: item.goodsCount, name: item.name }
-  })
-})
-const categoryGoodSale = computed(() => {
-  const goodsSale = analysisStore.categoryGoodsSale
-  const labels: string[] = []
-  const values: any[] = []
+const {
+  topPanelDatas,
+  categoryGoodsCount,
+  categoryGoodsFavor,
+  categoryGoodsSale,
+  goodsAddressSale
+} = storeToRefs(analysisStore)
 
-  for (const item of goodsSale) {
-    labels.push(item.name)
-    values.push(item.goodsCount)
-  }
-  return { labels, values }
-})
-const addressGoodSale = computed(() => {
-  return analysisStore.goodsAddressSale.map((item) => {
-    return { name: item.address, value: item.count }
-  })
+// get data
+const showGoodCatagoryCount = computed(() => {
+  return categoryGoodsCount.value.map((item) => ({
+    name: item.name,
+    value: item.goodsCount
+  }))
 })
 </script>
 
@@ -47,7 +41,7 @@ const addressGoodSale = computed(() => {
 .dashboard {
   background-color: #f5f5f5;
 
-  .row {
+  .el-row {
     margin-top: 20px;
   }
 }

7d34ad552682ae9c2d7a4ed20048db7ef76032cc
Xu <whitenight.xiaxu@gmail.com>
2024-12-04 15:04:15 +0800

    feat: category search

--- src/views/main/product/category/category.vue
+++ src/views/main/product/category/category.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="category">
-    <h2>category</h2>
+    <page-search
+      :search-config="searchFormConfig"
+      @query-click="handleQueryClick"
+      @reset-click="handleResetClick"
+    />
   </div>
 </template>
 
-<script setup lang="ts" name="category"></script>
+<script setup lang="ts" name="category">
+import pageSearch from '@/components/page-search/page-search.vue'
+import { searchFormConfig } from './config/search.config'
+import usePageContent from '@/hooks/usePageContent'
+
+const { contentRef, handleQueryClick, handleResetClick } = usePageContent()
+</script>
 
 <style scoped>
 .category {

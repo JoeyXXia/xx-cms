9955f423eb853d6883e1921027ce876364e9b725
Xu <whitenight.xiaxu@gmail.com>
2024-12-19 14:30:56 +0800

    feat: product category layout

--- src/views/main/product/category/category.vue
+++ src/views/main/product/category/category.vue
@@ -5,12 +5,17 @@
       @query-click="handleQueryClick"
       @reset-click="handleResetClick"
     />
+    <page-content ref="pageContentRef" :contentConfig="contentTableConfig"></page-content>
   </div>
 </template>
 
 <script setup lang="ts" name="category">
 import pageSearch from '@/components/page-search/page-search.vue'
+import PageContent from '@/components/page-content/page-content.vue'
+
 import { searchFormConfig } from './config/search.config'
+import { contentTableConfig } from './config/content.config'
+
 import usePageContent from '@/hooks/usePageContent'
 
 const { contentRef, handleQueryClick, handleResetClick } = usePageContent()

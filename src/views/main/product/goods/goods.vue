fb3f33dc37adfcccd4a589b35b5bdb44bdc6789f
Xu <whitenight.xiaxu@gmail.com>
2024-12-19 14:47:35 +0800

    feat: product goods layout

--- src/views/main/product/goods/goods.vue
+++ src/views/main/product/goods/goods.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="goods">
-    <h2>goods</h2>
+    <page-search
+      :search-config="searchFormConfig"
+      @query-click="handleQueryClick"
+      @reset-click="handleResetClick"
+    />
+
+    <page-content ref="pageContentRef" :content-config="contentTableConfig"></page-content>
   </div>
 </template>
 
-<script setup lang="ts" name="goods"></script>
+<script setup lang="ts" name="goods">
+import pageSearch from '@/components/page-search/page-search.vue'
+import pageContent from '@/components/page-content/page-content.vue'
+
+import { searchFormConfig } from './config/search.config'
+import { contentTableConfig } from './config/content.config'
+
+import usePageContent from '@/hooks/usePageContent'
+
+const { handleQueryClick, handleResetClick } = usePageContent()
+</script>
 
 <style scoped>
 .goods {
